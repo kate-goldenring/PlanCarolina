d77a80c3777f14fd6ec924f89ab1a03d9535431d
Hank Hester <hankhester@dhcp-v880-1-02386.1Xwireless.unc.edu>
2017-04-10 10:57:18 -0400

    added responsiveness for small screens

--- index.php
+++ index.php
@@ -32,6 +32,7 @@ if (!isset($_POST['username'])) {
   <script type="text/javascript" src="scripts/Sortable.js"> </script> 
   <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
   <link href="styles/style.css" rel="stylesheet" type="text/css">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Plan Carolina</title>
 </head>
 <body>

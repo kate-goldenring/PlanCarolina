9a4afbabe276fd55db4934097c61376cf10b688c
Brooks Townsend <brooksmtownsend@gmail.com>
2017-04-21 11:17:03 -0400

    Fixed PR, added analytics

--- index.php
+++ index.php
@@ -31,6 +31,17 @@ if (!isset($_POST['username'])) {
   <!sortable added in dragability and deletability of course items >
   <script type="text/javascript" src="scripts/Sortable.js"> </script> 
   <script type="text/json" src="majors/hankTesting.json"></script>
+  <script>//START GOOGLE ANALYTICS
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-97866143-1', 'auto');
+  ga('send', 'pageview');
+
+  </script>
+  <!--END GOOGLE ANALYTICS-->
   <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
   <link href="styles/style.css" rel="stylesheet" type="text/css">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
